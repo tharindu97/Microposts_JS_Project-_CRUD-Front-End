99984b17856144cc71165f7d53c80a37b4e0cb18
tharindu97 <tharindukavishna@gmail.com>
2019-12-07 16:44:54 +0530

    Add_Post_S

--- src/app.js
+++ src/app.js
@@ -5,8 +5,29 @@ import { ui } from './ui';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+//Listen for add Post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+//Get Posts
 function getPosts(){
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
-} 
+}
+
+// Submit Post
+function submitPost(){
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+    const data = {
+        title,
+        body
+    }
+
+    // Create Post
+    http.post('http://localhost:3000/posts', data)
+    .then(data => {
+        
+    })
+    .catch();
+}
